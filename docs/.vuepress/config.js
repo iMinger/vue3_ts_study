0c2fe284f18e6edb9ac9db008bd5d335d5bb189c
Minger <iMingerSky@gmail.com>
2021-05-06 11:21:25 +0800

    doc build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '/vue3_study/', /* 基础虚拟路径 */
+  base: '/vue3_ts_study/', /* 基础虚拟路径 */
   dest: 'docs/dist', /* 打包文件基础路径, 在命令所在目录下 */
   title: 'Vue3+TS 快速上手', // 标题
   description: '尚硅谷前端研究院', // 标题下的描述
